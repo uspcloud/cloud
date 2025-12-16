19b552b52d3354f6ec3604ac2b329459d523f98d
jack.sky0008 <jack.sky0008@gmail.com>
2025-12-16 12:18:52 +0000

    Add passwordEncodingTest code

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -4,9 +4,13 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -40,5 +44,17 @@ class DemoApplicationTests {
     public void adminPageLoadsWhenLoggedIn() throws Exception {
         mockMvc.perform(get("/admin"))
                 .andExpect(status().isOk());
+    }
+
+    @Test
+    public void passwordEncodingTest() {
+        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        String rawPassword = "password";
+        String encodedPassword = passwordEncoder.encode(rawPassword);
+        System.out.println("Encoded password: " + encodedPassword);
+
+        assertNotNull(encodedPassword);
+        assertNotEquals(rawPassword, encodedPassword);
+        assertTrue(passwordEncoder.matches(rawPassword, encodedPassword));
     }
 }
