8def9a21138a32107708a2837071be6b47468c73
jack.sky0008 <jack.sky0008@gmail.com>
2025-12-16 11:33:13 +0000

    Add test units

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,13 +1,44 @@
 package com.example.demo;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class DemoApplicationTests {
 
-  @Test
-  void contextLoads() {
-  }
+    @Autowired
+    private MockMvc mockMvc;
 
+    @Test
+    void contextLoads() {
+    }
+
+    @Test
+    public void loginPageLoads() throws Exception {
+        mockMvc.perform(get("/login"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void adminPageRedirectsWhenNotLoggedIn() throws Exception {
+        mockMvc.perform(get("/admin"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("http://localhost/login"));
+    }
+
+    @Test
+    @WithMockUser(username = "admin", roles = {"ADMIN"})
+    public void adminPageLoadsWhenLoggedIn() throws Exception {
+        mockMvc.perform(get("/admin"))
+                .andExpect(status().isOk());
+    }
 }
