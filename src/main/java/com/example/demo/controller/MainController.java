dac43f8367ec7c3488875b1f2447b312b7d116d7
jack.sky0008 <jack.sky0008@gmail.com>
2025-12-16 10:38:30 +0000

    upgrade to springboot4.0

---
+++ src/main/java/com/example/demo/controller/MainController.java
@@ -0,0 +1,23 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/")
+    public String home() {
+        return "home";
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    @GetMapping("/admin")
+    public String admin() {
+        return "admin";
+    }
+}
